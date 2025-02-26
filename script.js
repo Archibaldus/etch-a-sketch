08d39b74ebc9196f63158b7ccbfa96c901dc8d0a
Arthur <arthur.lukiewicz@gmail.com>
2025-02-26 07:01:23 +0100

    fixed layout and added stlye.css

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // create 12 divs
-for (let i = 0; i < 12; i++) {
+for (let i = 0; i < 256; i++) {
   const newDiv = document.createElement("div");
   newDiv.classList.add("box");
   document.querySelector(".container").appendChild(newDiv);
